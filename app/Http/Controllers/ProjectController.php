706f10dd4f7fc9cb34e78124f15f62b0effc3e8c
desyrahmi <desyrahmi09@gmail.com>
2016-05-21 19:08:49 +0700

    Update project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,6 +6,7 @@ use App\Project;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 use Validator;
 use Session;
@@ -62,5 +63,35 @@ class ProjectController extends Controller
         $project->delete();
 
         return redirect()->route('project.show');
+    }
+
+    public function showEditForm($id)
+    {
+        $project = Project::find($id);
+        return view('form.updateproject', ['project' => $project]);
+    }
+
+    public function update()
+    {
+        $rules = array(
+            'title' => 'required',
+            'description' => 'required',
+            'total' => 'required',
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+        $data = Input::all();
+        if ($validator->fails()) {
+            Session::flash('fail', 'Gagal mengupdate project');
+            return redirect()->route('project.edit.show',['id'=>$data['id']]);
+        }
+
+        $project = Project::find($data['id']);
+        DB::table('projects')->where('id' , $data['id'])->update([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'total' => $data['total'],
+        ]);
+        return redirect()->route('project.edit.show', ['id' => $data['id']]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,59 +15,61 @@
 //     return view('welcome');
 // });
 
-Route::get('mypage', function(){
-	return view('mypage');
+Route::get('mypage', function () {
+    return view('mypage');
 });
 
-Route::group(['middleware' => ['web']], function (){
-	Route::group(['middleware' => ['guest']], function (){
-		Route::get('/login', ['uses' => 'AuthController@index','as' => 'auth.index']);
-		Route::post('/login', ['uses' => 'AuthController@doLogin','as' => 'auth.doLogin']);
-	});
+Route::group(['middleware' => ['web']], function () {
+    Route::group(['middleware' => ['guest']], function () {
+        Route::get('/login', ['uses' => 'AuthController@index', 'as' => 'auth.index']);
+        Route::post('/login', ['uses' => 'AuthController@doLogin', 'as' => 'auth.doLogin']);
+    });
 
-	Route::group(['middleware' => ['auth']], function (){
-		Route::get('/logout', ['uses' => 'AuthController@doLogout','as' => 'auth.doLogout']);
-		
-		Route::group(['middleware' => ['role:Administrator']], function (){
-			Route::get('/user', ['uses' => 'UserController@index','as' => 'user.show']);
-			Route::get('/user/add', ['uses' => 'UserController@addIndex','as' => 'user.add.index']);
-			Route::post('/user/add', ['uses' => 'UserController@create','as' => 'user.add.create']);
-			Route::get('/user/edit/{id}', ['uses' => 'UserController@showEditForm','as' => 'user.edit.show']);
-			Route::post('/user/edit', ['uses' => 'UserController@update','as' => 'user.edit']);
-			Route::get('/user/delete/{id}', ['uses' => 'UserController@delete', 'as' => 'user.delete']);
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/logout', ['uses' => 'AuthController@doLogout', 'as' => 'auth.doLogout']);
 
-			Route::get('/project', ['uses' => 'ProjectController@index','as' => 'project.show']);
-			Route::get('/project/add', ['uses' => 'ProjectController@addIndex','as' => 'project.add.index']);
-			Route::post('/project/add', ['uses' => 'ProjectController@create','as' => 'project.add.create']);
-			Route::get('/project/delete/{id}', ['uses' => 'ProjectController@delete', 'as' => 'project.delete']);
+        Route::group(['middleware' => ['role:Administrator']], function () {
+            Route::get('/user', ['uses' => 'UserController@index', 'as' => 'user.show']);
+            Route::get('/user/add', ['uses' => 'UserController@addIndex', 'as' => 'user.add.index']);
+            Route::post('/user/add', ['uses' => 'UserController@create', 'as' => 'user.add.create']);
+            Route::get('/user/edit/{id}', ['uses' => 'UserController@showEditForm', 'as' => 'user.edit.show']);
+            Route::post('/user/edit', ['uses' => 'UserController@update', 'as' => 'user.edit']);
+            Route::get('/user/delete/{id}', ['uses' => 'UserController@delete', 'as' => 'user.delete']);
 
-			Route::get('/donate', ['uses' => 'DonateController@index','as' => 'donate.show']);
-			Route::get('/donate/add', ['uses' => 'DonateController@addIndex','as' => 'donate.add.index']);
-			Route::post('/donate/add', ['uses' => 'DonateController@create','as' => 'donate.add.create']);
-			Route::get('/donate/delete/{id}', ['uses' => 'DonateController@delete', 'as' => 'donate.delete']);
+            Route::get('/project', ['uses' => 'ProjectController@index', 'as' => 'project.show']);
+            Route::get('/project/add', ['uses' => 'ProjectController@addIndex', 'as' => 'project.add.index']);
+            Route::post('/project/add', ['uses' => 'ProjectController@create', 'as' => 'project.add.create']);
+            Route::get('/project/edit/{id}', ['uses' => 'ProjectController@showEditForm', 'as' => 'project.edit.show']);
+            Route::post('/project/edit', ['uses' => 'ProjectController@update', 'as' => 'project.edit']);
+            Route::get('/project/delete/{id}', ['uses' => 'ProjectController@delete', 'as' => 'project.delete']);
 
-			Route::get('/article', ['uses' => 'ArticleController@index','as' => 'article.show']);
-			Route::get('/article/add', ['uses' => 'ArticleController@addIndex','as' => 'article.add.index']);
-			Route::post('/article/add', ['uses' => 'ArticleController@create','as' => 'article.add.create']);
-			Route::get('/article/delete/{id}', ['uses' => 'ArticleController@delete', 'as' => 'article.delete']);
+            Route::get('/donate', ['uses' => 'DonateController@index', 'as' => 'donate.show']);
+            Route::get('/donate/add', ['uses' => 'DonateController@addIndex', 'as' => 'donate.add.index']);
+            Route::post('/donate/add', ['uses' => 'DonateController@create', 'as' => 'donate.add.create']);
+            Route::get('/donate/delete/{id}', ['uses' => 'DonateController@delete', 'as' => 'donate.delete']);
 
-			Route::get('/comment', ['uses' => 'CommentController@index','as' => 'comment.show']);
-			Route::get('/comment/add', ['uses' => 'CommentController@addIndex','as' => 'comment.add.index']);
-			Route::post('/comment/add', ['uses' => 'CommentController@create','as' => 'comment.add.create']);
-			Route::get('/comment/delete/{id}', ['uses' => 'CommentController@delete', 'as' => 'comment.delete']);
-			
-			Route::get('/event', ['uses' => 'EventController@index','as' => 'event.show']);
-			Route::get('/event/add', ['uses' => 'EventController@addIndex','as' => 'event.add.index']);
-			Route::post('/event/add', ['uses' => 'EventController@create','as' => 'event.add.create']);
-			Route::get('/event/delete/{id}', ['uses' => 'EventController@delete', 'as' => 'event.delete']);
+            Route::get('/article', ['uses' => 'ArticleController@index', 'as' => 'article.show']);
+            Route::get('/article/add', ['uses' => 'ArticleController@addIndex', 'as' => 'article.add.index']);
+            Route::post('/article/add', ['uses' => 'ArticleController@create', 'as' => 'article.add.create']);
+            Route::get('/article/delete/{id}', ['uses' => 'ArticleController@delete', 'as' => 'article.delete']);
+
+            Route::get('/comment', ['uses' => 'CommentController@index', 'as' => 'comment.show']);
+            Route::get('/comment/add', ['uses' => 'CommentController@addIndex', 'as' => 'comment.add.index']);
+            Route::post('/comment/add', ['uses' => 'CommentController@create', 'as' => 'comment.add.create']);
+            Route::get('/comment/delete/{id}', ['uses' => 'CommentController@delete', 'as' => 'comment.delete']);
+
+            Route::get('/event', ['uses' => 'EventController@index', 'as' => 'event.show']);
+            Route::get('/event/add', ['uses' => 'EventController@addIndex', 'as' => 'event.add.index']);
+            Route::post('/event/add', ['uses' => 'EventController@create', 'as' => 'event.add.create']);
+            Route::get('/event/delete/{id}', ['uses' => 'EventController@delete', 'as' => 'event.delete']);
 
 
-			Route::get('/photo', ['uses' => 'PhotoController@index','as' => 'photo.show']);
-			Route::get('/photo/add', ['uses' => 'PhotoController@addIndex','as' => 'photo.add.index']);
-			Route::post('/photo/add', ['uses' => 'PhotoController@create','as' => 'photo.add.create']);
-			Route::get('/photo/delete/{id}', ['uses' => 'PhotoController@delete', 'as' => 'photo.delete']);
-			Route::get('/photo/get/{fileName}', ['uses' => 'PhotoController@getPhoto', 'as' => 'photo.get']);
-		});
-	});
-	
+            Route::get('/photo', ['uses' => 'PhotoController@index', 'as' => 'photo.show']);
+            Route::get('/photo/add', ['uses' => 'PhotoController@addIndex', 'as' => 'photo.add.index']);
+            Route::post('/photo/add', ['uses' => 'PhotoController@create', 'as' => 'photo.add.create']);
+            Route::get('/photo/delete/{id}', ['uses' => 'PhotoController@delete', 'as' => 'photo.delete']);
+            Route::get('/photo/get/{fileName}', ['uses' => 'PhotoController@getPhoto', 'as' => 'photo.get']);
+        });
+    });
+
 });
