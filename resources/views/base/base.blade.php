bc81d32d66b35314af6c84e1caa80c71b8aaeb83
Rafiar rahmansyah <rafiar254@gmail.com>
2016-05-26 14:18:15 +0700

    edit base.blade

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
 	<link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet"/>
 	<link href="{{ URL::asset('css/style.css') }}" rel="stylesheet"/>
